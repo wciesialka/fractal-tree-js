c9689abb3022e6ec4b5fe337984286b89713cf7c
William Ciesialka <wciesialka@gmail.com>
2019-02-04 03:15:26 -0500

    Updated some ways of doing things

--- src/branch.js
+++ src/branch.js
@@ -1,40 +1,28 @@
-function Branch(parent, len, ang)
-{
+function Branch(parent, len, ang) {
     this.left = null;
     this.right = null;
     this.len = len;
-    if(parent == null)
-    {
-        this.startX = 300;
-        this.startY = 400;
+    if (parent == null) {
+        this.start = {x: 300, y: 400};
         this.ang = ang;
-    } else
-    {
-        this.startX = parent.endX;
-        this.startY = parent.endY;
+    } else {
+        this.start = parent.end;
         this.ang = (parent.ang + ang);
     }
-    this.endX = this.startX + (len * Math.cos(this.ang));
-    this.endY = this.startY + (len * Math.sin(this.ang));
+    this.end = {x: this.start.x + (len * Math.cos(this.ang)), y: this.start.y + (len * Math.sin(this.ang))};
 
-    this.grow = function(lAng, rAng)
-    {
-        var gLen = this.len * 0.6;
-        if(gLen > 2)
-        {
-            if(this.left == null)
-            {
+    this.grow = function (factor, lAng, rAng) {
+        var gLen = this.len * factor;
+        if (gLen > 2) {
+            if (this.left == null) {
                 this.left = new Branch(this, gLen, lAng);
-            } else
-            {
-                this.left.grow(lAng, rAng);
+            } else {
+                this.left.grow(factor, lAng, rAng);
             }
-            if(this.right == null)
-            {
+            if (this.right == null) {
                 this.right = new Branch(this, gLen, rAng);
-            } else
-            {
-                this.right.grow(lAng, rAng);
+            } else {
+                this.right.grow(factor, lAng, rAng);
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -1,65 +1,66 @@
-(function(window, document, undefined)
-{
+(function (window, document, undefined) {
     window.onload = init;
-})(window,document,undefined);
+})(window, document, undefined);
 
 var tree = null;
 
-function init()
-{
+function init() {
 
     var canvas = document.getElementById("canvas");
+
     var angle_form = document.getElementById("angle_form");
     var angle_span = document.getElementById("angle_span");
     angle_span.innerHTML = "Angle: " + angle_form.value;
-    var grow_button = document.getElementById("grow_button");
     var angle = parseFloat(angle_form.value);
-    angle_form.onchange = function(ev) {
+    angle_form.onchange = function (ev) {
         var src = ev.srcElement;
         angle = parseFloat(src.value);
         angle_span.innerHTML = "Angle: " + src.value;
     };
-    
-    tree = new Branch(null, 100, Math.PI + (Math.PI/2));
 
-    grow_button.onclick = function()
-    {
-        tree.grow(-angle, angle);
-        console.log(tree);
-    }
+    var grow_length_form = document.getElementById("grow_length_form");
+    var grow_length_span = document.getElementById("grow_length_span");
+    grow_length_span.innerHTML = "Grow Length Factor: " + grow_length_form.value;
+    var grow_length = parseFloat(grow_length_form.value);
+    grow_length_form.onchange = function (ev) {
+        var src = ev.srcElement;
+        grow_length = parseFloat(src.value);
+        grow_length_span.innerHTML = "Grow Length Factor: " + src.value;
+    };
 
-    tick = function(){
+    tree = new Branch(null, 100, Math.PI + (Math.PI / 2));
+
+    var grow_button = document.getElementById("grow_button");
+    grow_button.onclick = function () {
+        tree.grow(grow_length, -angle, angle);
+    };
+
+    tick = function () {
         draw(canvas);
     };
     window.setInterval(tick, 10);
 }
 
-function line(ctx, x1, y1, x2, y2)
-{
-    var sx = ctx.x;
-    var sy = ctx.y;
-    ctx.moveTo(x1,y1);
-    ctx.lineTo(x2,y2);
+function line(ctx, x1, y1, x2, y2) {
+    ctx.save();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
     ctx.stroke();
-    ctx.moveTo(sx,sy);
+    ctx.restore();
 }
 
-function draw(canvas)
-{
+function draw(canvas) {
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = "#332233";
-    ctx.fillRect(0,0,canvas.width, canvas.height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    drawBranch = function(branch)
-    {
+    drawBranch = function (branch) {
         ctx.strokeStyle = "#FFFFFF";
-        line(ctx,branch.startX, branch.startY, branch.endX, branch.endY);
-        if(branch.left != null)
-        {
+        line(ctx, branch.start.x, branch.start.y, branch.end.x, branch.end.y);
+        if (branch.left != null) {
             drawBranch(branch.left);
         }
-        if(branch.right != null)
-        {
+        if (branch.right != null) {
             drawBranch(branch.right);
         }
     }
